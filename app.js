89eb98224a30f6f1750655b1bf48b66f580ef65c
David Leigh <David Leigh@headoffice.trueclarity.net>
2018-12-13 08:36:33 +0000

    updated publish.js for sc9

--- app.js
+++ app.js
@@ -3,34 +3,49 @@ const fs = require('fs');
 const app = express();
 var child_process = require('child_process');
 
+// Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('mochawesome-report'));
 
 let isRunning = false;
-let state = "PENDING"
+let state = "IDLE"
+let lastStartTime = "";
 
 app.get('/', (req, res) =>
 {
     res.json({
 		"isRunning": isRunning,
 		"state" : state
-	})
+	});
+});
+
+app.get('/report', (req, res) =>
+{
+    res.sendFile(__dirname + '/mochawesome-report/mochawesome.html');
 });
 
 app.get('/run', (req, res) =>
-{
-    isRunning = true;
-	state = "PENDING";	
-	
-	res.json({
-		"isRunning": isRunning,
-		"state" : state,
-		"lastRunTime" : lastRunTime
-	});
-	
+{   	
 	child_process.exec('run.bat', function(error, stdout, stderr) {		
 		
 		isRunning = false;
 		state = "FINISHED";
 		
+	});
+	
+	isRunning = true;
+	state = "PENDING";	
+	lastStartTime = new Date().toISOString();
+	
+	res.json({
+		"isRunning": isRunning,
+		"state" : state,
+		"lastStartTime" : lastStartTime
 	});
 });
 
@@ -40,6 +55,12 @@ app.get('/results', (req, res) =>
 	
 	let rawData = fs.readFileSync('./mochawesome-report/mochawesome.json');  
 	let parsedData = JSON.parse(rawData);  
+	
+	parsedData.stats.status = { 
+		"isRunning": isRunning,
+		"state" : state,
+		"lastStartTime" : lastStartTime
+	};
 
 	verbose ? res.json(parsedData) : res.json(parsedData.stats);
 });
